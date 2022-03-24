d117054d1c31afba8531785d9766280b1e3e97de
marcus <marcmonk@gmail.com>
2022-03-24 19:30:41 -0400

    this was too easy...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -12,7 +13,11 @@ app.get('/', function (req, res) {
 });
 
 
-
+const upload = multer();
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // {"name":"vp.svg","type":"image/svg+xml","size":7213}
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
